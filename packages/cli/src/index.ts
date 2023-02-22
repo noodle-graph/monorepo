f1787c66f853332589b304c8a7f298bf36cba46b
Hila Rubin <hirubin@cisco.com>
2023-02-22 20:53:17 +0200

    change scan

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,7 +2,6 @@
 
 import { Command } from 'commander';
 
-import { bundle } from './bundle';
 import { here } from './here';
 import { scan } from './scan';
 import { printLogo } from './utils';
@@ -20,23 +19,12 @@ const hereCommand = new Command('here')
 const scanCommand = new Command('scan')
     .description('Using the scanner to search resources relationships.')
     .option('--config, -c <string>', 'Path to the scan configuration file.', './noodle.json')
-    .option('--output, -o <string>', 'Path of the output file.', './noodleScanOutput.json')
+    .option('--output, -o <string>', 'Path the the bundle output directory.','./noodleDist')
     .requiredOption('--githubToken <string>', 'GitHub access token.')
-    // .argument('[config]', 'Path to the scan configuration file.','./noodle.json')
-    // .argument('[output]', 'Path of the output file.','./noodleScanOutput.json')
-    // .argument('<githubToken>', 'GitHub access token.')
-    .action(scan);
-const bundleCommand = new Command('bundle')
-    .description('Creates a UI bundle with the specified scan output.')
-    // .argument('<name>', 'Name of the person to say goodbye to')
-    .description('Using the scanner to search resources relationships.')
-    .option('--scanOutput <string>', 'Path to the scan output file.','./noodleScanOutput.json')
-    .option('--output <string>', 'Path the the bundle output directory.','./noodleDist')
     .option('--open <boolen>', 'Whether to open in the browser the bundled UI when finished.','false')
-    .action(bundle);
+    .action(scan);
 
 program.addCommand(hereCommand);
 program.addCommand(scanCommand);
-program.addCommand(bundleCommand);
 
 program.parse(process.argv);
--- packages/cli/src/scan.ts
+++ packages/cli/src/scan.ts
@@ -1,9 +1,22 @@
 // import { scan_config } from "@noodle-graph/scanner";
+import { promises as fs } from 'fs';
+
 
 export async function scan(scanAttributes) {
     try {
         console.log(`In scan ${JSON.stringify(scanAttributes)}`)
-        // scan_config(scanAttributes.C,scanAttributes.O, scanAttributes.githubToken)
+        console.log(`In scan ${JSON.stringify(scanAttributes.C)}`)
+        console.log(`In scan ${JSON.stringify(scanAttributes.O)}`)
+
+        // scan_config(scanAttributes.C,scanAttributes.O + '/scanResult.json', scanAttributes.githubToken)
+
+        await fs.cp(__dirname + '/uiBuild', scanAttributes.O, {recursive: true});
+
+        const scanOutput = await fs.readFile(scanAttributes.O + '/scanResult.json','utf8')
+        let fileUiInput = await fs.readFile(scanAttributes.O + '/scanOutput.js','utf8');
+        fileUiInput = fileUiInput.replace('%SCAN_OUTPUT_PLACEHOLDER%', scanOutput)
+        await fs.writeFile(scanAttributes.O + '/scanOutput.js', fileUiInput, { encoding: 'utf8' });
+
     } catch (error) {
         console.error("Error occurred while reading the directory!", error);
     }
