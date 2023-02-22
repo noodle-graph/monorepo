a28e77a6fd21afaf063f2fff4c6fb9d26eba3cce
dormeiri <dormeiri@gmail.com>
2023-02-22 19:51:21 +0200

    ui filter fix

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -6,9 +6,11 @@ import { Pill } from './Pill';
 import { VisNetwork } from './VisNetowrk';
 
 export function App() {
-    const [selectedTags, setSelectedTags] = useState<string[]>([]);
     const [scanOutput, setScanOutput] = useState<any>();
-    const [tags, setTags] = useState<string[]>([]);
+    const [tags, setTags] = useState<any[]>([]);
+
+    const selectedTags = tags.filter((tag) => tag.selected);
+    const selectedTagValues = selectedTags.map((tag) => tag.value);
 
     useEffect(() => {
         const script = document.createElement('script');
@@ -19,30 +21,54 @@ export function App() {
             // @ts-expect-error
             const scanOutputNew = window.scanOutput;
             setScanOutput(scanOutputNew);
-            setTags([...new Set<string>(scanOutputNew.resources.flatMap((resource: any) => resource.tags ?? []))]);
+            const tagValues = [...new Set<string>(scanOutputNew.resources.flatMap((resource: any) => resource.tags ?? []))];
+            setTags(
+                tagValues.map((tag: any) => ({
+                    key: tag,
+                    value: tag,
+                    display: tag,
+                    selected: false,
+                }))
+            );
         };
         document.body.appendChild(script);
     }, []);
 
-    const tagsForFilter = tags.map((tag: any) => ({
-        key: tag,
-        value: tag,
-        display: tag,
-    }));
+    function handlePillClick(clickedTag) {
+        const newTags = JSON.parse(JSON.stringify(tags));
+
+        for (const tag of newTags) {
+            if (tag.value === clickedTag.value) {
+                tag.selected = false;
+            }
+        }
+
+        setTags(newTags);
+    }
+
+    function handleFilterChange(values) {
+        const newTags = JSON.parse(JSON.stringify(tags));
+
+        for (const tag of newTags) {
+            tag.selected = values.includes(tag.value);
+        }
+
+        setTags(newTags);
+    }
 
     return scanOutput == null ? (
         <div>Loading...</div>
     ) : (
         <div className="flex h-screen">
             <div className="w-96 bg-darker overflow-auto flex flex-col p-5">
-                <Filter options={tagsForFilter} onChange={(value) => setSelectedTags(value)} title="Tags" />
+                <Filter options={tags} onChange={handleFilterChange} title="Tags" />
                 <div className="flex flex-wrap text-xs gap-1 mt-1 rounded">
-                    {selectedTags.map((tag) => (
-                        <Pill onClick={() => setSelectedTags(selectedTags.filter((t) => t !== tag))} label={tag} />
+                    {selectedTags.map((tag, i) => (
+                        <Pill key={`pill-${i}`} onClick={() => handlePillClick(tag)} label={tag.value} />
                     ))}
                 </div>
             </div>
-            <div className="w-screen h-screen">{scanOutput && <VisNetwork scanOutput={scanOutput} selectedTags={selectedTags} />}</div>
+            <div className="w-screen h-screen">{scanOutput && <VisNetwork scanOutput={scanOutput} selectedTags={selectedTagValues} />}</div>
         </div>
     );
 }
