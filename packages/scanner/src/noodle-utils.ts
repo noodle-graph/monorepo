2a3751466cef1904f44759d62ccf2ce28cc91179
Erez Weinstein <125476602+erezweinstein5@users.noreply.github.com>
2023-02-22 17:02:39 +0200

    Scanner (#4)
    * npm link fix
    
    * regex to find noodles

--- packages/scanner/src/noodle-utils.ts
+++ packages/scanner/src/noodle-utils.ts
@@ -1,4 +1,8 @@
-const regex = 'dasdas';
+import * as console from "console";
+
+const regex = '/noodle.+';
+
+let noodleRegEx: RegExp = /noodle.+/;
 
 type Finding = {
     source: string;
@@ -6,11 +10,12 @@ type Finding = {
     match: string;
 };
 
-export async function relationships(path: string, content: string) {
+export function relationships(path: string, content: string) {
     const comments: Finding[] = [];
     const lines = content.split('\n');
     lines.forEach((line, i) => {
-        const matches = line.match(regex);
+        // const matches = line.match(regex);
+        const matches = noodleRegEx.exec(line);
         if (matches) {
             comments.push({
                 source: path,
@@ -27,3 +32,17 @@ export function persist(resources, path: string) {
     console.log(`persisting ${resources} to $ ${path}`);
     return '';
 }
+
+const testContent = 'export class NotificationClientWebex implements NotificationClient {\n' +
+    '    @inject(TYPE.HTTP_CLIENT) @named(NAME.WEBEX_API) private readonly client!: HttpClient;\n' +
+    '\n' +
+    '    // noodle --publish to-> webex (post,discovery-scan,discovery-scan-init,discovery-scan-control)\n' +
+    '    @tryCatch.tryCatch(undefined, undefined, false)\n' +
+    '    async notify(markdown: string): Promise<void> {\n' +
+    '        await this.client.post({ url: \'\', data: { roomId: notificationSettings.roomId, markdown } });'
+
+
+let commentsTest;
+commentsTest = relationships('file_nane', testContent);
+console.log(commentsTest);
+console.log('fsdfsdf');
