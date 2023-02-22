0e866c6b1e018a2bef7a4c1f1269ffb7cc2fafd0
Erez Weinstein <erezw55@gmail.com>
2023-02-22 14:10:25 +0200

    writeToJSONFile function

--- packages/scanner/src/index.ts
+++ packages/scanner/src/index.ts
@@ -1,3 +1,23 @@
+import {writeFile} from 'fs';
+// @ts-ignore
+import {Resource} from "../../ui/src/VisNetowrk";
+
 export function foo() {
     console.log('foo');
 }
+
+export async function writeToJSONFile(resourcesList: any) {
+    const path = process.cwd() + '/scan_config_file.json';
+    await new Promise((resolve) => writeFile(path, JSON.stringify(resourcesList, null, 2), resolve));
+}
+
+const resourceList: Resource[] = [
+    {
+        id: "test_id",
+        type: "test_type",
+        name: "test_name",
+        tags: ["tag1", "tag2"],
+        relationships: [{resourceId: "test_resourceId", action: "test_action", url:"https://test.url/test", tags: ["tag3", "tag4"]}],
+    }
+]
+writeToJSONFile(resourceList);
