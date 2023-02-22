428cbee3fe4075605d55cbd0f6d74ebb03ecd20c
Erez Weinstein <erezw55@gmail.com>
2023-02-22 14:30:10 +0200

    fix

--- packages/scanner/src/index.ts
+++ packages/scanner/src/index.ts
@@ -1,5 +1,6 @@
 import {writeFile} from 'fs';
-import {Resource} from "../../types/src";
+
+import {Resource} from '../../types/src';
 
 export function foo() {
     console.log('foo');
@@ -12,11 +13,12 @@ export async function writeToJSONFile(resourcesList: any) {
 
 const resourceList: Resource[] = [
     {
-        id: "test_id",
-        type: "test_type",
-        name: "test_name",
-        tags: ["tag1", "tag2"],
-        relationships: [{resourceId: "test_resourceId", action: "test_action", url:"https://test.url/test", tags: ["tag3", "tag4"]}],
-    }
-]
+        id: 'test_id',
+        type: 'test_type',
+        name: 'test_name',
+        tags: ['tag1', 'tag2'],
+        relationships: [{resourceId: 'test_resourceId', action: 'test_action', url: 'https://test.url/test', tags: ['tag3', 'tag4']}],
+    },
+];
+
 writeToJSONFile(resourceList);
