c3849801e769c007acba85ca222d0f13603ff6bd
dormeiri <dormeiri@gmail.com>
2023-03-03 17:02:34 +0200

    organize tests

---
+++ packages/scanner/src/__mocks__/gitClientMock.ts
@@ -0,0 +1,14 @@
+import { GitClient } from '../gitClient';
+
+interface GitCloneOptions {
+    repoUrl: string;
+    localUrl: string;
+    branch: string;
+    token: string;
+}
+
+export class GitClientMock extends GitClient {
+    override clone(_options: GitCloneOptions): Promise<void> {
+        return Promise.resolve();
+    }
+}
