268a2c111418e75230967d1b490b09adeb8f32b8
dormeiri <dormeiri@gmail.com>
2023-03-12 23:54:32 +0200

    serve as API

--- packages/cli/src/run.ts
+++ packages/cli/src/run.ts
@@ -1,55 +1,39 @@
-import { exec } from 'child_process';
 import { readFile, cp, writeFile, mkdir } from 'fs/promises';
 import { join, isAbsolute } from 'path';
 
 import { scan } from '@noodle-graph/scanner';
-import loggerConstructor from 'pino';
+
+import { serve } from './serve';
+import { createLogger } from './utils';
 
 const UI_BUILD_DIR_PATH = '../ui/build';
 const SCAN_OUTPUT_JS_FILENAME = 'scanOutput.js';
 const SCAN_OUTPUT_JS_PLACEHOLDER = "'%SCAN_OUTPUT_PLACEHOLDER%'";
-const INDEX_HTML_FILENAME = 'index.html';
 
-export async function run(scanAttributes): Promise<void> {
-    const logger = createLogger(scanAttributes.verbose ? 'debug' : 'info');
+export async function run(attributes): Promise<void> {
+    const logger = createLogger(attributes.verbose ? 'debug' : 'info');
 
-    logger.debug(scanAttributes, 'Running command `run`');
+    logger.debug(attributes, 'Running command `run`');
 
-    const configPath = isAbsolute(scanAttributes.C) ? scanAttributes.C : join(process.cwd(), scanAttributes.C);
-    const outputDirPath = isAbsolute(scanAttributes.O) ? scanAttributes.O : join(process.cwd(), scanAttributes.O);
-    const uiDirPath = join(outputDirPath);
-    const uiIndexPath = join(uiDirPath, INDEX_HTML_FILENAME);
-    const scanOutputJsPath = join(uiDirPath, SCAN_OUTPUT_JS_FILENAME);
+    const configPath = isAbsolute(attributes.C) ? attributes.C : join(process.cwd(), attributes.C);
+    const outputDirPath = isAbsolute(attributes.O) ? attributes.O : join(process.cwd(), attributes.O);
+    const scanOutputJsPath = join(outputDirPath, SCAN_OUTPUT_JS_FILENAME);
 
     const scanResult = await scan(
         {
             config: JSON.parse((await readFile(configPath)).toString()),
-            github: { token: scanAttributes.githubToken },
-            scanWorkersNum: scanAttributes.workers,
+            github: { token: attributes.githubToken },
+            scanWorkersNum: attributes.workers,
         },
         logger
     );
 
-    await mkdir(uiDirPath, { recursive: true });
-    await cp(join(__dirname, UI_BUILD_DIR_PATH), uiDirPath, { recursive: true });
+    await mkdir(outputDirPath, { recursive: true });
+    await cp(join(__dirname, UI_BUILD_DIR_PATH), outputDirPath, { recursive: true });
 
     const scanOutputUiTemplate = (await readFile(scanOutputJsPath)).toString();
     const scanOutputUi = scanOutputUiTemplate.replace(SCAN_OUTPUT_JS_PLACEHOLDER, JSON.stringify(scanResult));
     await writeFile(scanOutputJsPath, scanOutputUi, { encoding: 'utf8' });
 
-    if (scanAttributes.open === true) {
-        exec(`open ${uiIndexPath}`);
-    }
-}
-
-function createLogger(level: 'debug' | 'info') {
-    return loggerConstructor({
-        level,
-        transport: {
-            target: 'pino-pretty',
-            options: {
-                ignore: 'pid,githubToken',
-            },
-        },
-    });
+    if (attributes.open) serve({ scanOutputDir: outputDirPath, open: true, host: '127.0.0.1' });
 }
